98f5f93f1bb6fdb393ec046eb1e9ebc52967e4ea
HackerX-47 <omkar121207@gmail.com>
2025-10-30 18:19:40 +0530

    Updated

--- Dynamic_Quiz_Game/Dynamic_Quiz_Game.c
+++ Dynamic_Quiz_Game/Dynamic_Quiz_Game.c
@@ -1,1 +1,52 @@
+#include<stdio.h>
 
+
+enum Choice{
+    PLAY_GAME,
+    ADD_QUESTION,
+    EXIT
+};
+void instructions();
+void playGame(FILE *fPtr);
+void addQuestion(FILE *fPtr);
+void checkAnswer(FILE *fPtr);
+
+int main(void){
+    FILE *fPtr=NULL;
+    fPtr=fopen("Questions.txt","a+");
+    int choice=0;
+    scanf("%d",&choice);
+    while(choice!=0){
+        switch(choice){
+            case EXIT:
+                {
+                    // opted for terminating program
+                    break;
+                }
+            case PLAY_GAME:
+                {
+                    playGame(fPtr);
+                    break;
+                }
+            case ADD_QUESTION:
+                {
+                    addQuestion(fPtr);
+                    break;
+                }
+            default:
+                {
+                    // wrong option opted
+                    instructions();
+                }
+        }
+        if(choice!=0){
+            scanf("%d",&choice);
+        }
+    }
+    printf("Program terminating ...");
+}
+
+void addQuestion(FILE *fPtr){
+    rewind(fPtr);
+    
+}
