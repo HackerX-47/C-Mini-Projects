1d32793d43bfc580de1640d94bc6ca20963a8390
HackerX-47 <omkar121207@gmail.com>
2025-10-31 11:17:00 +0530

    Final Quiz Game Upload

--- Dynamic_Quiz_Game/Dynamic_Quiz_Game.c
+++ Dynamic_Quiz_Game/Dynamic_Quiz_Game.c
@@ -1,46 +1,90 @@
+/*
+---------------------------------------------
+Program : Dynamic Quiz Game
+Author  : Omkar
+Date    : October 2025
+---------------------------------------------
+Description:
+A fully dynamic quiz game in C that allows users to:
+ - Play a quiz with preloaded questions.
+ - Add their own custom questions.
+ - Randomize questions each time the quiz is played.
+
+Each question contains 4 options and one correct answer.
+Data is stored in "Questions.txt" for reading and writing.
+---------------------------------------------
+*/
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include<ctype.h>
 #include<time.h>
 
+
+/*
+--------------------------------------------------------------------------------
+structure to store questions, respective 4 options and 1 answer to each question
+--------------------------------------------------------------------------------
+*/
 struct question
 
 {
 
-    char question[201];
-    char *options[4]; // a,b,c,d,\0
-    char answer;
+    char question[201]; // stores the question
+    char *options[4]; // array of pointers stores the four options of each question
+    char answer; // stores a char value index of answer i.e., A / B / C / D
 
 } ;
 
 typedef struct question Question;
 
+/*
+--------------------------
+Enum for user menu choices
+--------------------------
+*/
 enum Choice 
 
 {
 
-    PLAY_GAME,
-    ADD_QUESTION,
-    EXIT
+    EXIT, // choice = 0
+    PLAY_GAME, // choice= 1
+    ADD_QUESTION // choice = 2
+    
 
 } ;
 
-void instructions ( ) ;
-void playGame ( FILE *fPtr , int addedQuestionCounter ) ;
-void addQuestion ( FILE *fPtr ) ;
-void preloadQuestions ( FILE *fPtr ) ;
+// Function Declarations
+void instructions ( ) ;                                   // displays the instructions to play the game
+void playGame ( FILE *fPtr , int addedQuestionCounter ) ; // playGame function prototype , which handles process of playing the game
+void addQuestion ( FILE *fPtr ) ;                         // addQuestion function prototype , which handles process of adding new questions in the game
+void preloadQuestions ( FILE *fPtr ) ;                    // preloadQuestions function prototype , which loads the 10 fixed questions in the game
+void playGameInstructions();                              // playGameInstructions show how to play the game
+void addQuestionInstructions();                           // addQuestionInstructions show how to add question in game
 
+// starting of main function body
 int main ( void ) 
 
 {
 
-    int addCounter = 0 ;
-    int choice = 0 ;
-    scanf( "%d" , &choice ) ;
+    int addCounter = 0 ; // counter which counts the number of added questions
+    int choice = 0 ; // variable which stores the value of choice opted by the user
 
-    FILE *fPtr = NULL ;
-    fPtr = fopen ( "Questions.txt" , "w+" ) ;
+    FILE *fPtr = NULL ; // fPtr which handles the processes related to file Questions.txt
+    fPtr = fopen ( "Questions.txt" , "w+" ) ; // fPtr opens up the file
+
+    printf ( "+---------------------------------------+\n" ) ;
+    printf ( "|           Dynamic Quiz Game           |\n" ) ;
+    printf ( "+---------------------------------------+\n\n" ) ;
+
+    instructions ( ) ;
+
+    printf ( "CHOICE :- " ) ;
+    scanf( "%d" , &choice ) ;
+    printf("\n");
+
+    
     
     while ( choice != 0 )
     
@@ -54,7 +98,6 @@ int main ( void )
                 
                 {
 
-                    // opted for terminating program
                     break ;
 
                 }
@@ -63,6 +106,9 @@ int main ( void )
                 
                 {
 
+                    printf ( "--------------------------------\n" ) ;
+                    printf ( "          Game Begins           \n" ) ;
+                    printf ( "--------------------------------\n" ) ;
                     preloadQuestions ( fPtr ) ;
                     playGame ( fPtr , addCounter ) ;
                     break ;
@@ -73,6 +119,9 @@ int main ( void )
 
                 {
 
+                    printf ( "--------------------------------\n" ) ;
+                    printf ( "         Add Questions          \n" ) ;
+                    printf ( "--------------------------------\n" ) ;
                     ++addCounter ;
                     addQuestion ( fPtr ) ;
                     break ;
@@ -83,24 +132,26 @@ int main ( void )
 
                 {
 
-                    // wrong option opted
-                    instructions ( ) ;
+                    printf ( "Invalid Choice \nTry Again \n" ) ;
 
                 }
 
         }
 
+        instructions ( ) ;
+
         if ( choice != 0 )
         
         {
 
+            printf ( "\nCHOICE :- " ) ;
             scanf ( "%d" , &choice ) ;
 
         }
 
     }
 
-    printf ( "Program terminating ..." ) ;
+    printf ( "Program terminating ...\n\n" ) ;
 
 }
 
@@ -110,24 +161,29 @@ void playGame ( FILE *fPtr , int addedQuestionCounter )
 
 {
 
+    playGameInstructions();
     rewind ( fPtr ) ;
     Question game1 ;
 
-    int totalQuestions = 9 + addedQuestionCounter ;
+    int totalQuestions = 10 + addedQuestionCounter ;
     srand ( time ( NULL ) ) ;
 
     int score = 0 ;
     int random [ totalQuestions ] ;
 
     for ( int i = 0 ; i < totalQuestions ; ++i )
+    
+    {
+
         random [ i ] = -1 ;
+
+    }
 
     for ( int queCounter = 0 ; queCounter < totalQuestions ; ++queCounter ) 
     
     {
 
-        int j , match ;
-        // generate unique random index
+        int j = 0 , match = 0 ;
 
         do 
         
@@ -157,51 +213,102 @@ void playGame ( FILE *fPtr , int addedQuestionCounter )
 
         random [ queCounter ] = j ;  // mark used index
 
-        // jump to question j
+        
 
         rewind ( fPtr ) ;
 
         char buffer [ 201 ] ;
 
+        // jump to question j
         for ( int skip = 0 ; skip < j * 6 ; ++skip )
+        
+        {
+
             fgets ( buffer , sizeof ( buffer ) , fPtr ) ;
 
-        // now read question j
+        }
+        
 
         char question [ 201 ] ; 
-        fgets ( question , sizeof(question) , fPtr ) ;
+        fgets ( question , sizeof ( question )  , fPtr ) ;
 
         question [ strcspn ( question , "\n" ) ] = '\0' ;
         printf ( "\n%s\n" ,  question ) ;
 
-        for ( int i = 0 ; i < 4 ; ++i ) 
+        for ( int i = 0 ; i < 4 ; ++ i ) 
         
         {
 
             game1 . options [ i ] = malloc ( 101 * sizeof ( char ) ) ;
+            if ( game1 . options [ i ] == NULL )
+            
+            {
+
+                printf ( "Not enough memory available." ) ;
+
+            }
+            
+            else
+            
+            {
+
             fgets ( game1 . options [ i ] , 100, fPtr ) ;
             game1 . options [ i ] [ strcspn ( game1 . options [ i ] , "\n" ) ] = '\0' ;
             printf ( "%c. %s\n" , 'A' + i , game1 . options [ i ] ) ;
+
+            }
 
         }
 
         char answer = '\0' , choice = '\0' ;
         fscanf ( fPtr , " %c" , &answer ) ;
 
-        printf ( "Your choice: " ) ;
+        printf ( "\nYour choice: " ) ;
         scanf ( " %c" , &choice ) ;
 
         choice = toupper ( choice ) ;
 
+        while( choice != 'A' && choice != 'B' && choice != 'C' && choice != 'D' )
+    
+        {
+
+        printf ( "Invalid Answer\n" ) ;
+
+        printf ( "Answer ( i.e., A , B , C , D ) :- " ) ;
+        scanf ( " %c" , &choice ) ;
+
+        choice = toupper ( choice ) ;
+
+        }
+
         if ( choice == answer ) 
+        
+        {
+            
+            printf ( "Correct Answer \n\n" ) ;
             ++ score ;
+        
+        }
+
+        else 
+
+        {
+
+            printf ( "Wrong Answer \n\n" ) ;
+
+        }
 
         for ( int i = 0 ; i < 4 ; ++ i )
+        
+        {
+
             free ( game1 . options [ i ] ) ;
+
+        }
 
     }
 
-    printf ( "\nFinal Score: %d\n" , score ) ; 
+    printf ( "\nFinal Score: %d out of %d questions\n" , score, totalQuestions ) ; 
 
 }
 
@@ -211,13 +318,17 @@ void addQuestion ( FILE *fPtr )
 
 {
 
+    addQuestionInstructions ( ) ;
+
     int c ;
     while ( ( c = getchar ( ) ) != '\n' && c != EOF ) ; // flush input buffer
 
     Question add = { 0 } ;
-    fgets ( add . question , sizeof ( add . question ) , stdin ) ;
 
+    printf ( "Question :- " ) ;
+    fgets ( add . question , sizeof ( add . question ) , stdin ) ;
     add.question [ strcspn ( add . question , "\n" ) ] = '\0' ; // remove newline
+
     fprintf ( fPtr , "%s\n" , add . question ) ;
 
     for ( int i = 0 ; i < 4 ; ++ i )
@@ -225,9 +336,24 @@ void addQuestion ( FILE *fPtr )
     {
 
         add . options [ i ] = malloc ( 101 * sizeof ( char ) ) ;   // allocate space
-        fgets ( add . options [ i ] , 100 , stdin ) ;             // read full line (including spaces)
-        add . options [ i ] [ strcspn ( add . options [ i ] , "\n" ) ] = '\0' ;  // remove trailing newline
+        if ( add . options [ i ] == NULL )
+            
+        {
+                
+            printf ( "Not enough memory available." ) ;
+
+        }
+            
+        else
+            
+        {
+
+            printf ( "Option %d :- " , ( i + 1 ) ) ;
+            fgets ( add . options [ i ] , 100 , stdin ) ;              // read full line (including spaces)
+            add . options [ i ] [ strcspn ( add . options [ i ] , "\n" ) ] = '\0' ;  // remove trailing newline
     
+        }
+
     }
 
     for ( int i = 0 ; i < 4 ; ++ i )
@@ -238,11 +364,23 @@ void addQuestion ( FILE *fPtr )
 
     }
 
-    int c ;
-    while ( ( c = getchar ( ) ) != '\n' && c != EOF ) ;  // clear leftover newline
-
+    printf ( "Answer ( i.e., A , B , C , D ) :- " ) ;
     scanf ( " %c" , &add.answer ) ;
+
     add . answer = toupper ( add . answer ) ;
+
+    while( add . answer != 'A' && add . answer != 'B' && add . answer != 'C' && add . answer != 'D' )
+    
+    {
+
+        printf ( "Invalid Answer\n" ) ;
+
+        printf ( "Answer ( i.e., A , B , C , D ) :- " ) ;
+        scanf ( " %c" , &add.answer ) ;
+
+        add . answer = toupper ( add . answer ) ;
+
+    }
 
     fprintf ( fPtr , "%c\n" , add . answer ) ;
     
@@ -298,4 +436,45 @@ void preloadQuestions ( FILE *fPtr )
 
     ) ;
     
+}
+
+void instructions ( ) 
+
+{
+
+    printf ( "Enter one of the following choices:\n" ) ;
+    printf ( "0 - Exit the program\n" ) ;
+    printf ( "1 - Play the quiz game\n" ) ;
+    printf ( "2 - Add a new question\n\n" ) ;
+
+}
+
+void playGameInstructions ( )
+
+{
+
+    printf ( "\n---------------------------------------------\n" ) ;
+    printf ( "        HOW TO PLAY THE QUIZ GAME\n" ) ;
+    printf ( "---------------------------------------------\n" ) ;
+    printf ( "1. The quiz consists of preloaded and added questions.\n" ) ;
+    printf ( "2. Each question has 4 options - A, B, C, and D.\n" ) ;
+    printf ( "3. Type the letter (A, B, C, or D) for your chosen answer.\n" ) ;
+    printf ( "4. The quiz automatically randomizes question order each time.\n" ) ;
+    printf ( "5. Your final score will be shown at the end.\n\n" ) ;
+
+}
+
+void addQuestionInstructions ( )
+
+{
+
+    printf ( "\n---------------------------------------------\n" ) ;
+    printf ( "         HOW TO ADD NEW QUESTIONS\n" ) ;
+    printf ( "---------------------------------------------\n" ) ;
+    printf ( "1. Enter a question when prompted.\n" ) ;
+    printf ( "2. Then enter four options (one per line).\n" ) ;
+    printf ( "3. After all options, specify the correct answer (A/B/C/D).\n" ) ;
+    printf ( "4. Added questions are stored temporarily during this session.\n" ) ;
+    printf ( "   (They reset if you restart the program.)\n\n" ) ;
+
 }
